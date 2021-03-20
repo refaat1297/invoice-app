099f06010e085457540871096ff9f5a435e7bd82
refaat1297 <a.refaat1297@gmail.com>
2021-03-20 10:37:46 +0200

    add invoice head

--- pages/invoice/_id/index.vue
+++ pages/invoice/_id/index.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>
-        <h1>{{ $route.params.id }}</h1>
-        <button @click="deleteInvoiceHandler">
-            <span v-if="loading">loading</span>
-            <span v-else>click me</span>
-        </button>
+    <div class="invoice-page">
+       <div class="container">
+           <div class="row">
+               <div class="col-12 col-lg-11" >
+                   <div class="invoice-head">
+                       <h2>
+                           <span>#</span><span>{{ this.$route.params.id }}</span>
+                       </h2>
+
+                       <div class="actions">
+                           <nuxt-link :to="`/invoice/${$route.params.id}/edit`">Edit</nuxt-link>
+                           <button @click="deleteInvoiceHandler">
+                               <b-spinner
+                                   v-if="loading"
+                                   variant="white"
+                               ></b-spinner>
+                               <span v-else>delete</span>
+                           </button>
+                       </div>
+                   </div>
+
+               </div>
+           </div>
+       </div>
     </div>
 </template>
 
@@ -43,6 +61,72 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.invoice-page {
+    padding: 5rem 0;
 
+    .invoice-head {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+
+        h2 {
+            margin-bottom: 0;
+
+            span {
+                &:first-of-type {
+                    font-size: 2rem;
+                    color: var(--gray-color);
+                }
+
+                &:last-of-type {
+                    text-transform: uppercase;
+                    font-size: 1.5rem;
+                }
+            }
+        }
+
+        .actions {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+
+            a {
+                height: 50px;
+                background-color: #252946;
+                color: #fff;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                user-select: none;
+                cursor: pointer;
+                width: 110px;
+                text-decoration: none;
+                border-radius: 25px;
+                text-transform: capitalize;
+            }
+
+
+            button {
+                width: 110px;
+                height: 50px;
+                background-color: #ff4757;
+                color: #fff;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                border-radius: 25px;
+                user-select: none;
+                cursor: pointer;
+                border: none;
+                text-transform: capitalize;
+                margin-left: .5rem;
+
+                &:focus {
+                    outline: none;
+                }
+            }
+        }
+    }
+}
 </style>
