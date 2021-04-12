753ab1d79edb0939741bfa07a537517899d66806
refaat1297 <a.refaat1297@gmail.com>
2021-04-12 18:53:55 +0200

    change nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@
 
 export default {
     // Target: https://go.nuxtjs.dev/config-target
-    target: 'universal',
+    // target: 'universal',
 
     ssr: true,
     // Global page headers: https://go.nuxtjs.dev/config-head
